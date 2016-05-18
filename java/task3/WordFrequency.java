11794e8322367eb0fe94d292b002478166b6dd4e
Adm1nistrator <dolgikh.alexey@gmail.com>
2016-05-18 18:00:07 +0600

    переделал цикл поиск одинаковых слов

--- java/task3/WordFrequency.java
+++ java/task3/WordFrequency.java
@@ -17,40 +17,25 @@ public class WordFrequency {
        /* BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = bufferedReader.readLine();  */
         String fileName = "task3.txt";
-        String[] words = new String[0];
+        String[] words = {};
         String input;
         Map<String, Integer> wordFrequencyMap = new HashMap<>();
         BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
         while ((input = fileReader.readLine()) != null) {
             Pattern p = Pattern.compile("[^\\wа-яА-Я]+");
             words = p.split(input);
-         /*   words = input.split(" ");*/
-            int i = 1;
-            wordFrequencyMap.put(words[i], 1);
-        }
-        Integer count ;
-        for (String word: words)
-        {
-            count=wordFrequencyMap.get(word);
-            wordFrequencyMap.put(word,count==null ? 1 : count+1);
+            Integer count;
+            for (String word : words) {
+                count = wordFrequencyMap.get(word);
+                wordFrequencyMap.put(word, count == null ? 1 : count + 1);
 
-        }
-
-
-/*
-        int count = 0;
-        for (String word : words) {
-            for (String otherWord : words) {
-                if (otherWord.equals(word)) {
-                    count++;
-                }
             }
-            wordFrequencyMap.put(word, count);
-            count = 0;
-        }*/
+        }
+
+
 
         for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
-            System.out.println("Слово: " + entry.getKey() + " Частота: " + entry.getValue() + " Частота: "+((100*entry.getValue())/words.length)+" %");
+            System.out.println("Слово: " + entry.getKey() + " Частота: " + entry.getValue() + " Частота: " + ((100 * entry.getValue()) / words.length) + " %");
         }
 
 
