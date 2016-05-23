6e9e658c11f977acfbedbd670b6c4c62b72dd789
Adm1nistrator <dolgikh.alexey@gmail.com>
2016-05-23 15:37:21 +0600

    Реализован метод  printStackTrace и первые три исключения.

--- java/task5/Exceptions.java
+++ java/task5/Exceptions.java
@@ -4,24 +4,27 @@ package task5;
  * Created by anykey on 23.05.16.
  */
 public class Exceptions implements ExceptionGenerator {
-    public static void main(String[] args) {
-
-    }
 
     @Override
     public void generateNullPointerException() {
-        String a;
+        Integer a = null;
+        String string = a.toString();
     }
 
     @Override
     public void generateClassCastException() {
+        Object x = new Integer(0);
+        String string = (String) x;
 
     }
 
     @Override
     public void generateNumberFormatException() {
+        String string = "2O0"; //  в Числе 2O0, один знако-это буква O.
+        Integer integer = Integer.parseInt(string);
 
     }
+
 
     @Override
     public void generateStackOverflowError() {
@@ -30,11 +33,55 @@ public class Exceptions implements ExceptionGenerator {
 
     @Override
     public void generateOutOfMemoryError() {
-
     }
 
     @Override
     public void generateMyException(String message) throws MyException {
+
+    }
+
+    public static void printStackTrace(Throwable throwable) {
+        System.out.println(throwable);
+        for (StackTraceElement element : throwable.getStackTrace()) {
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Exceptions exceptions = new Exceptions();
+        try {
+            exceptions.generateNullPointerException();
+        } catch (Exception e) {
+           printStackTrace(e);
+        }
+        try {
+            exceptions.generateClassCastException();
+        } catch (Exception e) {
+
+            printStackTrace(e);
+        }
+
+        try {
+            exceptions.generateNumberFormatException();
+
+        } catch (Exception e) {
+            printStackTrace(e);
+        }
+        try {
+            exceptions.generateStackOverflowError();
+
+        } catch (Exception e) {
+            printStackTrace(e);
+        }
+        try {
+            exceptions.generateOutOfMemoryError();
+
+        } catch (Exception e) {
+
+            printStackTrace(e);
+        }
 
     }
 }
